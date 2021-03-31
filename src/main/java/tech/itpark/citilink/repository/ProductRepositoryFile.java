4c69d3a7e9b9d9fcd859689cffa0c6cc3f5fff17
Anastasiia787 <nastenaprokopeva787@gmail.com>
2021-04-01 00:08:16 +0300

    add repository

---
+++ src/main/java/tech/itpark/citilink/repository/ProductRepositoryFile.java
@@ -0,0 +1,22 @@
+package tech.itpark.citilink.repository;
+
+import tech.itpark.citilink.domain.Product;
+
+import java.util.ArrayList;
+
+public class ProductRepositoryFile implements ProductRepository {
+    @Override
+    public ArrayList<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Product product) {
+
+    }
+
+    @Override
+    public void removeById(long id) {
+
+    }
+}
